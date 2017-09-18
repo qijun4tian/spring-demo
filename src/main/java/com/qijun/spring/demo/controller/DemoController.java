fe3bef7cfbf5d3003a49960eaa940dfc1c160cec
e <424767284@qq.com>
2017-09-18 11:45:36 +0800

    second commit

--- src/main/java/com/qijun/spring/demo/controller/DemoController.java
+++ src/main/java/com/qijun/spring/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.qijun.spring.demo.controller;
 
 import com.qijun.spring.demo.service.UserPasswordService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by qijun123 on 2017/9/14.
  */
 @RestController
+@Slf4j
 public class DemoController {
     @Autowired
     private UserPasswordService userPasswordService;
     @RequestMapping(path ="/greeting/{name}")
     public String greeting(@PathVariable("name") String name) {
+        log.info("hello world");
         return "hello, "+name+" your password is "+userPasswordService.getUserPassword(name);
     }
 }
